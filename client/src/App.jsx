882ae29565385bbf45b3b71ad66022b40008ece6
Anshuman <107368860+anshuman008@users.noreply.github.com>
2024-03-11 13:02:14 +0530

    intial commit

---
+++ client/src/App.jsx
@@ -0,0 +1,93 @@
+import { useEffect, useMemo, useState } from "react";
+import {io} from "socket.io-client";
+import {Box, Button, Container, TextField, Typography} from '@mui/material';
+import './App.css'; // Import your custom CSS
+
+const App = () => {
+
+const socket =useMemo(()=>  io("http://localhost:3000"),[]);
+
+const [allmessages,setallmessages] = useState([]);
+const [message,setMessage] = useState("");
+const [socketId,setsocketId] = useState("");
+const [room,setRoom] = useState("");
+const [roomName,setRoomName] = useState("");
+
+
+const handleSumit = (e) =>{
+  e.preventDefault(); 
+  socket.emit("message",{message,room});
+  setMessage("");
+}
+
+const joinRoomHandler =(e) =>{
+  e.preventDefault(); 
+  socket.emit('join-room',roomName);
+  setRoomName("");
+}
+
+console.log(allmessages);
+useEffect(()=>{
+    socket.on("connect",()=>{
+      console.log('connect ho gya bro',socket.id);
+      setsocketId(socket.id) 
+    })
+
+    socket.on('receive-message',(data)=>{
+      console.log(data);
+      setallmessages((allmessages) => [...allmessages,data]);
+    })
+
+    socket.on("welcome",(s)=>{
+      console.log(s)
+    })
+},[])
+
+  return (
+    <Container className="chat-container">
+            <Box className="socket-id-display">
+                <Typography variant="h6">Your ID: {socketId}</Typography>
+            </Box>
+
+            <form onSubmit={joinRoomHandler} className="room-form">
+                <TextField 
+                    value={roomName}
+                    onChange={(e) => setRoomName(e.target.value)}
+                    label="Room Name"
+                    variant="outlined"
+                    className="input-field"
+                    autoComplete="false"
+                />
+                <Button type="submit" variant="contained" color="primary">Join Room</Button>
+            </form>
+
+            <form onSubmit={handleSumit} className="message-form">
+                <TextField 
+                    value={message}
+                    onChange={(e) => setMessage(e.target.value)}
+                    label="Message"
+                    variant="outlined"
+                    className="input-field"
+                    autoComplete="false"
+                />
+                <TextField 
+                    value={room}
+                    onChange={(e) => setRoom(e.target.value)}
+                    label="User Id"
+                    variant="outlined"
+                    className="input-field"
+                    autoComplete="false"
+                />
+                <Button type="submit" variant="contained" color="primary">Send</Button>
+            </form>
+
+            <Box className="message-display">
+                {allmessages.map((m, index) => (
+                    <Typography key={index} marginTop={2} className="message">{m}</Typography>
+                ))}
+            </Box>
+        </Container>
+  )
+}
+
+export default App
---
+++ server/app.js
@@ -0,0 +1,45 @@
+import express from "express";
+import { Server } from "socket.io";
+import { createServer } from "http";
+import cors from "cors";
+
+
+const port = 3000;
+const app = express();
+
+app.use(cors()); // CORS middleware should be used before defining routes
+
+
+const server = createServer(app);
+
+
+const io = new Server(server, {
+    cors: {
+        origin: "http://localhost:5173", // The frontend origin
+        methods: ["GET", "POST"],
+        credentials: true // Corrected the case
+    }
+});
+
+io.on("connection", (socket) => {
+    console.log("User Connected, Id:", socket.id);
+    // socket.emit("welcome",`welcome to the server ${socket.id}`)
+    socket.broadcast.emit("welcome",`welcome to the server ${socket.id}`);
+
+    socket.on("message",({message,room})=>{
+    // console.log(data);
+    socket.to(room).emit("receive-message",message);
+    })
+
+    socket.on('join-room',(room)=>{
+        socket.join(room);
+    })
+});
+
+app.get("/", (req, res) => {
+    res.send('Hello World');
+});
+
+server.listen(port, () => {
+    console.log(`Server started on port: ${port}`);
+});
