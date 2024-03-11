55be293c0c27acb4c323978f70c1ab65d75ef4be
Anshuman <107368860+anshuman008@users.noreply.github.com>
2024-03-11 13:03:28 +0530

    Update CORS origin in app.js

--- server/app.js
+++ server/app.js
@@ -15,7 +15,7 @@ const server = createServer(app);
 
 const io = new Server(server, {
     cors: {
-        origin: "http://localhost:5173", // The frontend origin
+        origin: "*", // The frontend origin
         methods: ["GET", "POST"],
         credentials: true // Corrected the case
     }
